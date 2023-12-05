455ac6890bcc551e7ff58b00773fe956d667a027
HE XIANGDONG <88825510+hexiangdong0@users.noreply.github.com>
2023-12-05 11:35:05 +0800

    Update shared_memory_lib.c

--- shared_memory_lib.c
+++ shared_memory_lib.c
@@ -3,22 +3,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/file.h>
+#include <stdint.h>
+#include <string.h>
 
 int readSharedMemory(const char *lockfile, const char *shmem, char *msg, int msg_size) {
+    // msg_size 读取成功
+    // -1 打开lockfile失败
+    // -2 上锁失败
+    // -3 打开shmem失败
+    // -4 读取shmem失败
     int lock_fd, shmem_fd;
 
     // Open the lock file
     lock_fd = open(lockfile, O_RDONLY);
     if (lock_fd < 0) {
         perror("Error opening lock file");
-        return 0;
+        return -1;
     }
 
     // Acquire an exclusive lock
     if (flock(lock_fd, LOCK_EX) == -1) {
         perror("Locking failed");
         close(lock_fd);
-        return 0;
+        return -2;
     }
 
     // Open the shared memory file
@@ -27,12 +34,13 @@ int readSharedMemory(const char *lockfile, const char *shmem, char *msg, int msg
         perror("Error opening shared memory file");
         flock(lock_fd, LOCK_UN);
         close(lock_fd);
-        return 0;
+        return -3;
     }
 
     // Read the message from shared memory
     if (read(shmem_fd, msg, msg_size) != msg_size) {
         perror("Error reading shared memory");
+        return -4;
     }
 
     // Release the lock and close files
@@ -43,21 +51,25 @@ int readSharedMemory(const char *lockfile, const char *shmem, char *msg, int msg
 }
 
 int writeToSharedMemory(const char *lockfile, const char *shmem, const char *msg, int msg_size) {
-// void writeToSharedMemory(unsigned int value) {
+    // msg_size 写入成功
+    // -1 打开锁文件失败
+    // -2 上锁失败
+    // -3 打开shmem失败
+    // -4 写入shmem失败
     int lock_fd, shmem_fd;
 
     // Open the lock file, create if it doesn't exist
     lock_fd = open(lockfile, O_WRONLY | O_CREAT, 0666);
     if (lock_fd < 0) {
         perror("Error opening/creating lock file");
-        return 0;
+        return -1;
     }
 
     // Acquire an exclusive lock
     if (flock(lock_fd, LOCK_EX) == -1) {
         perror("Locking failed");
         close(lock_fd);
-        return 0;
+        return -2;
     }
 
     // Open the shared memory file, create if it doesn't exist
@@ -66,12 +78,70 @@ int writeToSharedMemory(const char *lockfile, const char *shmem, const char *msg
         perror("Error opening/creating shared memory file");
         flock(lock_fd, LOCK_UN);
         close(lock_fd);
-        return 0;
+        return -3;
     }
 
     // Write the value to shared memory
     if (write(shmem_fd, msg, msg_size) != msg_size) {
         perror("Error writing to shared memory");
+        return -4;
+    }
+
+    // Release the lock and close files
+    flock(lock_fd, LOCK_UN);
+    close(shmem_fd);
+    close(lock_fd);
+    return msg_size;
+}
+
+
+int checkAndWrite(const char *lockfile, const char *shmem, const char *condition, const char *msg, int msg_size) {
+    // msg_size shmem内容和condition相等，并且写入成功
+    // 0 shmem内容和condition不相等
+    // -1 无法打开锁文件
+    // -2 上锁失败
+    // -3 无法打开shmem
+    // -4 无法读取shmem
+    // -5 无法写入shmem
+    int lock_fd, shmem_fd;
+    char buff[1024];
+
+    // Open the lock file
+    lock_fd = open(lockfile, O_RDONLY);
+    if (lock_fd < 0) {
+        perror("Error opening lock file");
+        return -1;
+    }
+
+    // Acquire an exclusive lock
+    if (flock(lock_fd, LOCK_EX) == -1) {
+        perror("Locking failed");
+        close(lock_fd);
+        return -2;
+    }
+
+    // Open the shared memory file
+    shmem_fd = open(shmem, O_RDWR);
+    if (shmem_fd < 0) {
+        perror("Error opening shared memory file");
+        flock(lock_fd, LOCK_UN);
+        close(lock_fd);
+        return -3;
+    }
+
+    // Read the message from shared memory
+    if (read(shmem_fd, buff, msg_size) != msg_size) {
+        perror("Error reading shared memory");
+        return -4;
+    }
+
+    if (strncmp(buff, condition, msg_size) != 0) {
+        return 0;
+    }
+
+    if (write(shmem_fd, msg, msg_size) != msg_size) {
+        perror("Error writing shared memory");
+        return -5;
     }
 
     // Release the lock and close files
